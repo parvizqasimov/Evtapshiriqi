5259a40277860f1d2cb570f334cf65f8628c2554
parvizqasimov <119752037+parvizqasimov@users.noreply.github.com>
2022-12-17 00:02:40 +0400

    first commit

---
+++ Evtapshiriqi1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+int a = int.Parse(Console.ReadLine());
+if (a >= 0 && a <= 9) { a = 1; }
+else if (a >= 10 && a <= 99) { a = 2; }
+else if (a >= 100 && a <= 999) { a = 3; }
+else if (a >= 1000 && a <= 9999) { a = 4; }
+else if (a >= 10000 && a <= 99999) { a = 5; }
+else if (a >= 100000 && a <= 999999) { a = 6; }
+
+else { Console.WriteLine("Yazilan reqem MILYONDA COXDUR"); };
+
+
+Console.WriteLine(a);
